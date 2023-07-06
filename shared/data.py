fdd8d73a34af4e8c90bff62ded3f5ab20e7595f8
rickdott <rickdotyt@gmail.com>
2023-07-06 13:23:00 +0200

    Updated awaiting HMP update

--- shared/data.py
+++ shared/data.py
@@ -1,13 +1,18 @@
+SAT1 = 0
+SAT2 = 1
+AR = 2
+
 # Select data from path, can select based on conditions or participants, by default all participants are selected
 class DataWrangler():
     # List of data sets that should be loaded and kept in memory
     def __init__(paths):
         ...
 
-    def select_data(path, conditions, participants=None):
+    def select_data(path, conditions=None, participants=None):
 
         ...
 
     # Input iterable of datasets (output of select_data), return combined version
     def combine_data(sets):
         ...
+
--- shared/utilities.py
+++ shared/utilities.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+# import tensorflow as tf
 
 SAT1_STAGES_ACCURACY = ['encoding', 'decision', 'confirmation', 'response']
 SAT1_STAGES_SPEED = ['encoding', 'decision', 'response']
@@ -6,13 +6,13 @@ SAT2_STAGES_ACCURACY = ['encoding', 'decision', 'confirmation', 'response']
 SAT2_STAGES_SPEED = ['encoding', 'decision', 'response']
 AR_STAGES = ['encoding', 'familiarity', 'memory', 'decision', 'response']
 
-earlyStopping_cb = tf.keras.callbacks.EarlyStopping(
-    monitor="val_loss",
-    min_delta=0,
-    patience=2,
-    verbose=0,
-    mode="auto",
-    baseline=None,
-    restore_best_weights=True,
-    start_from_epoch=0,
-)
+# earlyStopping_cb = tf.keras.callbacks.EarlyStopping(
+#     monitor="val_loss",
+#     min_delta=0,
+#     patience=2,
+#     verbose=0,
+#     mode="auto",
+#     baseline=None,
+#     restore_best_weights=True,
+#     start_from_epoch=0,
+# )
