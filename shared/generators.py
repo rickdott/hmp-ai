537b29552b38906b109d214028297ff6cf1cc5d9
rickdott <rickdotyt@gmail.com>
2023-08-22 13:36:58 +0200

    Updated training and generator to use XArray Dataset

--- shared/generators.py
+++ shared/generators.py
@@ -2,35 +2,60 @@ import tensorflow as tf
 import numpy as np
 
 
-class NewSAT1DataGenerator(tf.keras.utils.Sequence):
-    pass
-
-
 class SAT1DataGenerator(tf.keras.utils.Sequence):
-    def __init__(self, data, labels, batch_size=16):
-        # Shuffle data and labels
-        np.random.seed(42)
-        perm = np.random.permutation(len(data))
-        # TODO: Investigate if normalization is necessary
-        data = (data - np.min(data)) / (np.max(data) - np.min(data))
-        self.data = data[perm]
-        self.labels = labels[perm]
-        self.categories = sorted(list(set(self.labels.flatten())))
-        self.labels_cat = np.array(
-            [self.categories.index(label) for label in self.labels]
+    # Pass XArray dataset, already filtered
+    def __init__(self, dataset, batch_size=16):
+        # Dataset can contain NaN samples
+        self.dataset = dataset
+        # Normalizes to [0,1] TODO: Investigate if necessary or if [-1, 1] is better
+        self.dataset = self.dataset.fillna(0)
+        self.dataset = (self.dataset.data - np.min(self.dataset.data)) / (
+            np.max(self.dataset.data) - np.min(self.dataset.data)
         )
-
+        
+        self.n_participants = len(dataset.participant)
+        self.n_epochs = len(dataset.epochs)
+        self.n_labels = len(dataset.labels)
+        self.cat_labels = sorted(list(set(dataset.labels.to_numpy())))
         self.batch_size = batch_size
 
+        # Find indices of NaN slices
+        mask = self.dataset.isnull().all(dim=["channels", "samples"])
+        nan_slices = np.argwhere(mask.data)
+        cartesian = np.array(
+            np.meshgrid(
+                np.arange(self.n_participants),
+                np.arange(self.n_epochs),
+                np.arange(self.n_labels),
+            )
+        ).T.reshape(-1, 3)
+        filtered_indices = np.array(
+            [row for row in cartesian if row.tolist() not in nan_slices.tolist()]
+        )
+        self.n_valid_slices = filtered_indices.shape[0]
+
+        # Shuffle valid indices
+        np.random.seed(42)
+        perm = np.random.permutation(self.n_valid_slices)
+        self.filtered_indices = filtered_indices[perm]
+
     def __len__(self):
-        return int(np.ceil(self.data.shape[0] / float(self.batch_size)))
+        return int(np.ceil(self.n_valid_slices / float(self.batch_size)))
 
     def __getitem__(self, idx):
-        batch = self.data[idx * self.batch_size : (idx + 1) * self.batch_size, :, :, :]
-
-        labels_cat = self.labels_cat[
-            idx * self.batch_size : (idx + 1) * self.batch_size
+        batch_indices = self.filtered_indices[
+            idx * self.batch_size : (idx + 1) * self.batch_size, :
         ]
+        dim1_indices = batch_indices[:, 0]
+        dim2_indices = batch_indices[:, 1]
+        dim3_indices = batch_indices[:, 2]
 
-        # Shape (16, 30, 210), (, 16)
-        return batch, labels_cat
+        batch_data = self.dataset[
+            dim1_indices, dim2_indices, dim3_indices, slice(None), slice(None)
+        ]
+        batch_labels = batch_data.labels  # Labels as strings
+        batch_labels = np.array([self.cat_labels.index(label) for label in batch_labels])
+
+        # Returns data: (batch_size, channels, max sample length) and labels: (batch_size, )
+        # return batch_data.data[0, 0, :, :, :], batch_labels
+        return (batch_data.data[0, 0, :, :, :].reshape(-1, 30, 157, 1), batch_labels)
