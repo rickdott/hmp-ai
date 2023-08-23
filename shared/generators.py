9a2096521b64650acd5418062f0b352e06ba0d8b
rickdott <rickdotyt@gmail.com>
2023-08-23 13:12:13 +0200

    Reworked generator to use xbatcher and xarray only

--- shared/generators.py
+++ shared/generators.py
@@ -1,5 +1,53 @@
 import tensorflow as tf
 import numpy as np
+import xbatcher
+
+
+class NewSAT1DataGenerator(tf.keras.utils.Sequence):
+    def __init__(self, dataset, batch_size=16):
+        # Alphabetical ordering of labels used for categorization of labels
+        self.cat_labels = list(dataset.labels)
+
+        self.batch_size = batch_size
+        # Preprocess data
+        # Stack three dimensions into one MultiIndex dimension 'index'
+        dataset = dataset.stack({"index": ["participant", "epochs", "labels"]})
+        # Reorder so that index is at the front
+        dataset = dataset.transpose("index", ...)
+        # Drop all indices for which all channels & samples are NaN
+        # measuring error or label does not occur under condition in dataset
+        dataset = dataset.dropna("index", how="all")
+        dataset = dataset.fillna(0)
+
+        # Normalize to [0,1] TODO: Investigate if necessary or if [-1, 1] is better
+        dataset = (dataset.data - np.min(dataset.data)) / (
+            np.max(dataset.data) - np.min(dataset.data)
+        )
+
+        # Reshuffle
+        np.random.seed(42)
+        perm = np.random.permutation(len(dataset.index))
+        dataset = dataset[perm]
+
+        # Create xbatcher generator
+        self.generator = xbatcher.BatchGenerator(
+            dataset,
+            input_dims={
+                "channels": len(dataset.channels),
+                "samples": len(dataset.samples),
+            },
+            batch_dims={"index": batch_size},
+        )
+
+    def __len__(self):
+        return self.generator.__len__()
+
+    def __getitem__(self, idx):
+        batch = self.generator.__getitem__(idx)
+        batch_labels = np.array(
+            [self.cat_labels.index(label) for label in batch.labels]
+        )
+        return (batch.data, batch_labels)
 
 
 class SAT1DataGenerator(tf.keras.utils.Sequence):
@@ -7,8 +55,9 @@ class SAT1DataGenerator(tf.keras.utils.Sequence):
     def __init__(self, dataset, batch_size=16):
         # Dataset can contain NaN samples
         self.dataset = dataset
-        # Normalizes to [0,1] TODO: Investigate if necessary or if [-1, 1] is better
+        # TODO: Fills all NaN values with 0, even where whole slices are 0
         self.dataset = self.dataset.fillna(0)
+        # Normalizes to [0,1] TODO: Investigate if necessary or if [-1, 1] is better
         self.dataset = (self.dataset.data - np.min(self.dataset.data)) / (
             np.max(self.dataset.data) - np.min(self.dataset.data)
         )
