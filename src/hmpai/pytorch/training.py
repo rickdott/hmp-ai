fe7effef64e973f79112fe414009741f8b2d8c74
Rick den Otter <r.denotter@uu.nl>
2026-03-27 15:30:21 +0100

    Turned off pe

--- src/hmpai/pytorch/training.py
+++ src/hmpai/pytorch/training.py
@@ -32,7 +32,7 @@ def mixed_collate(batch):
     max_label_len = max(y.shape[0] for y in ys)
     max_label_width = max(y.shape[1] for y in ys)
 
-    max_coord_channels = max_channels - 1
+    max_coord_channels = max_channels
     
     # Pad sequences to maximum length
     padded_xs = []
