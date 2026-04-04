9ca40123c141055922fcb7091c840f2620beae39
Rick den Otter <r.denotter@uu.nl>
2026-04-04 14:17:13 +0200

    removed gradient norm

--- src/hmpai/pytorch/training.py
+++ src/hmpai/pytorch/training.py
@@ -394,7 +394,6 @@ def train(
                     }
                 )
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         scheduler.step()
 
